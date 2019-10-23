9edb75add9d387085d1b7f2a6c15eee5723315b9
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-10-23 20:12:36 +0200

    Reorganize dev profile definition
    The goal is to let profiles.clj file inside the project directory
    override some settings on demand (e.g., inject CIDER dependencies for
    different versions)
    
    But never commit profiles.clj to the repository, to avoid
    "contaminating" other developers with local preferences/settings.

--- project.clj
+++ project.clj
@@ -16,8 +16,9 @@
                                       :password :env/clojars_password
                                       :sign-releases false}]]
   :profiles
-  {:dev {:plugins [[jonase/eastwood "0.3.4"]
-                   [lein-cljfmt "0.6.2"]]}
+  {:dev [:project/dev :profiles/dev]
    :repl {:repl-options {:host "0.0.0.0"
-                         :port 4001}
-          :plugins [[cider/cider-nrepl "0.18.0"]]}})
+                         :port 4001}}
+   :profiles/dev {}
+   :project/dev {:plugins [[jonase/eastwood "0.3.4"]
+                           [lein-cljfmt "0.6.2"]]}})
