16539e71976f159d76863bf651b3fb4ac42ee4b3
Iñaki Arenaza <inaki.arenaza@biotz.io>
2024-05-30 11:05:14 +0200

    Release v0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/encryption "0.2.4-SNAPSHOT"
+(defproject dev.gethop/encryption "0.2.4"
   :description "Encryption/decryption library"
   :url "https://github.com/gethop-dev/encryption"
   :license {:name "Mozilla Public Licence 2.0"
